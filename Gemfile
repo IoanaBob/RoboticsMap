75401bf99633236d683944c05b55a87ba3b0214f
Ioana Surdu-Bob <ioana.sbob@gmail.com>
2018-03-24 21:17:53 +0200

    update

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 ruby '2.5.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
+
 # Postgres
 gem 'pg'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -29,12 +29,10 @@ module RoboticsMap
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
 
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:8000'
-      resource '*', :headers => :any, :methods => [:get, :post, :options]
-    end
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
   end
 end
