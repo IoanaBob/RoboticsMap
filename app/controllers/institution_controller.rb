971f47fce3fd6d67885f3b1ef98a91d57c611264
Ioana Surdu-Bob <ioana.sbob@gmail.com>
2018-03-24 15:41:34 +0200

    institution endpoints

---
+++ app/controllers/institution_controller.rb
@@ -0,0 +1,35 @@
+class InstitutionController < ApplicationController
+
+  def index
+    institutions = Institution.all
+    render json: institutions, status: :ok
+  end
+
+  def show
+    institution = Institution.find(params[:id])
+    render json: institution, status: :ok
+  end
+
+  def create
+    category = Category.find(params[:category_id])
+    if category.blank?
+      render json: { errors: [category: 'category does not exist'] }, status: :not_found
+    else
+      institution = category.institutions.new(institution_params)
+
+      if institution.save
+        head :ok
+      else
+        render json: institution.errors, status: :bad_request
+      end
+    end
+  end
+
+  private
+
+  def institution_params
+    params
+      .require(:institution)
+      .permit(:address, :phone, :email, :name, :contact_name, :description, :member_count, :women_count)
+  end
+end
